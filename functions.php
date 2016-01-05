47bdef9e3b20fa7c34877dd928ee030b3bcebcac
miya0001 <miya@wpist.me>
2016-01-05 23:56:45 +0900

    some fix

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 if ( ! isset( $content_width ) )
 	$content_width = 750;
 
-define( 'UMINCHU_SCRIPTS_VERSION', 'v0.1.25' );
+define( 'UMINCHU_SCRIPTS_VERSION', 'v0.1.26' );
 
 load_theme_textdomain( 'uminchu', get_stylesheet_directory() . '/languages' );
 
